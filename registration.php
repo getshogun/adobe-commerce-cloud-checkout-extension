087ad413ff6f415b0300947af2f190498898b760
Hugo Sequeira <hugo@getshogun.com>
2022-06-17 10:50:47 -0600

    INT-1018: Bootstrap module

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © Shogun, Inc. All rights reserved.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Shogun_FrontendCheckout',
+    __DIR__
+);
