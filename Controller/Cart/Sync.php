ccce8a48bca9c11e59c3b25c11cbe028bb808b43
Hugo Sequeira <hugo@getshogun.com>
2022-06-20 17:44:32 -0600

    INT-1020: Sync cart to logged user

--- Controller/Cart/Sync.php
+++ Controller/Cart/Sync.php
@@ -2,24 +2,74 @@
 
 namespace Shogun\FrontendCheckout\Controller\Cart;
 
-class Sync extends \Magento\Framework\App\Action\Action
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Integration\Model\Oauth\Token;
+use Magento\Integration\Model\Oauth\TokenFactory;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Quote\Model\QuoteFactory;
+use Magento\Quote\Model\QuoteRepository;
+use Magento\Checkout\Model\Session;
+use Magento\Framework\Controller\Result\RedirectFactory;
+
+class Sync implements HttpGetActionInterface
 {
     /**
-     * @var \Magento\Framework\View\Result\PageFactory
+     * @var PageFactory
      */
-    protected $resultPageFactory;
+    protected $pageFactory;
 
     /**
-     * @param \Magento\Framework\App\Action\Context $context
-     * @param \Magento\Framework\View\Result\PageFactory resultPageFactory
+     * @var RequestInterface
      */
+    protected $request;
+
+    protected $tokenFactory;
+    protected $customerRepository;
+    /**
+     * @var CustomerSession
+     */
+    protected $customerSession;
+    protected $quoteRepository;
+    /**
+     * @var Session
+     */
+    protected $checkoutSession;
+    /**
+     * @var QuoteFactory
+     */
+    protected $quoteFactory;
+    /**
+     * @var RedirectFactory
+     */
+    protected $redirectFactory;
+    protected $logger;
+
     public function __construct(
-        \Magento\Framework\App\Action\Context      $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        PageFactory $pageFactory,
+        RequestInterface $request,
+        TokenFactory $tokenFactory,
+        CustomerRepositoryInterface $customerRepository,
+        CustomerSession $customerSession,
+        QuoteRepository $quoteRepository,
+        QuoteFactory $quoteFactory,
+        Session $checkoutSession,
+        RedirectFactory $redirectFactory
     )
     {
-        $this->resultPageFactory = $resultPageFactory;
-        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+        $this->request = $request;
+        $this->tokenFactory = $tokenFactory;
+        $this->customerRepository = $customerRepository;
+        $this->customerSession = $customerSession;
+        $this->quoteRepository = $quoteRepository;
+        $this->quoteFactory = $quoteFactory;
+        $this->checkoutSession = $checkoutSession;
+        $this->redirectFactory = $redirectFactory;
     }
 
     /**
@@ -29,8 +79,56 @@ class Sync extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
-        echo $this->getRequest()->getParam('cartId');
-        echo $this->getRequest()->getParam('token');
-        exit;
+        $cartId = $this->request->getParam('cartId');
+        $customerTokenParam = $this->request->getParam('token');
+
+        // 1. Get Token from Token Key
+        $token = $this->tokenFactory->create()->loadByToken($customerTokenParam);
+
+        if (!$token->getId()) {
+            echo "User not found", PHP_EOL;
+            exit;
+        }
+
+        // 2. Get Customer from Token
+        $customer = $this->customerRepository->getById($token->getCustomerId());
+
+        // 3. Login customer
+        $this->customerSession->loginById($customer->getId());
+
+        // 4. Get Customer Quote or Cart Quote
+        try {
+            $quote = $this->quoteRepository->getForCustomer($customer->getId());
+        } catch (NoSuchEntityException $e) {
+            echo "Error: No quote!", $e->getMessage(), PHP_EOL;
+            $quote = $this->quoteFactory->create();
+
+            // TODO: TEST THIS!
+            try {
+                $cartQuote = $this->quoteRepository->getActive($cartId);
+            } catch (NoSuchEntityException $e) {
+                // TODO: REMOVE DEBUG LOG
+                echo "Error: No cart quote!", $e->getMessage(), PHP_EOL;
+                // $this->logger->error($e->getMessage());
+                return $this->redirectFactory->create()->setPath("checkout");
+            }
+
+            // Merge Cart Quote into Customer Quote, if necessary
+            if ($quote->getId() !== $cartQuote->getId()) {
+                $quote->assignCustomerWithAddressChange(
+                    $customer,
+                    $cartQuote->getBillingAddress(),
+                    $cartQuote->getShippingAddress()
+                );
+                $this->quoteRepository->save($quote->merge($cartQuote)->collectTotals());
+            }
+        }
+
+        //5. Sync checkout to quote
+        $quote->collectTotals();
+        $this->checkoutSession->replaceQuote($quote);
+
+        // 6. Redirect to checkout
+        return $this->redirectFactory->create()->setPath("checkout");
     }
 }
--- README.md
+++ README.md
@@ -3,8 +3,22 @@ Adobe Commerce Cloud extension to checkout orders from Shogun storefronts.
 
 ## Usage
 
-bin/magento module:enable Shogun_FrontendCheckout –clear-static-content
+Clone this repo into your Magento's instance `src/app/code/Shogun/` and run:
+
+```bash
+bin/magento module:enable Shogun_FrontendCheckout --clear-static-content
 
 bin/magento setup:upgrade
 
 bin/magento cache:flush
+```
+
+## Injector
+
+Magento uses a tool named "Injector" to inject dependencies into constructors.
+Hence, if you ever change your constructor signature, you need to recompile
+in order to inject the necessary dependencies by running:
+
+```bash
+bin/magento setup:upgrade && bin/magento cache:clean && bin/magento setup:di:compile
+```
