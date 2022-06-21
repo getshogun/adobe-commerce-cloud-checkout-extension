49c23a7ed188f8517c2c0133ab711d37755b3de2
Hugo Sequeira <hugo@getshogun.com>
2022-06-21 15:59:36 -0600

    INT-1020: Sync signed in carts

--- Controller/Cart/Sync.php
+++ Controller/Cart/Sync.php
@@ -1,134 +1,75 @@
 <?php
 
+/**
+ *
+ * Copyright © Shogun, Inc. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
 namespace Shogun\FrontendCheckout\Controller\Cart;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Integration\Model\Oauth\Token;
-use Magento\Integration\Model\Oauth\TokenFactory;
 use Magento\Framework\App\RequestInterface;
-use Magento\Framework\View\Result\Page;
-use Magento\Framework\View\Result\PageFactory;
-use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Customer\Model\Session as CustomerSession;
-use Magento\Quote\Model\QuoteFactory;
-use Magento\Quote\Model\QuoteRepository;
-use Magento\Checkout\Model\Session;
 use Magento\Framework\Controller\Result\RedirectFactory;
+use Shogun\FrontendCheckout\Service\CheckoutSync;
 
+/**
+ * Handles GET requests to sync and checkout a customer cart
+ */
 class Sync implements HttpGetActionInterface
 {
     /**
-     * @var PageFactory
+     * Name of the parameter holding the customer's access token
      */
-    protected $pageFactory;
-
+    const SHOGUN_FRONTEND_CHECKOUT_TOKEN_PARAM = 'token';
+    /**
+     * Name of the parameter with the cart ID
+     */
+    const SHOGUN_FRONTEND_CHECKOUT_CART_PARAM = 'cartId';
+    /**
+     * Checkout URI path
+     */
+    const SHOGUN_FRONTEND_CHECKOUT_CHECKOUT_PATH = 'checkout';
     /**
      * @var RequestInterface
      */
     protected $request;
-
-    protected $tokenFactory;
-    protected $customerRepository;
-    /**
-     * @var CustomerSession
-     */
-    protected $customerSession;
-    protected $quoteRepository;
-    /**
-     * @var Session
-     */
-    protected $checkoutSession;
-    /**
-     * @var QuoteFactory
-     */
-    protected $quoteFactory;
     /**
      * @var RedirectFactory
      */
     protected $redirectFactory;
-    protected $logger;
+    /**
+     * @var CheckoutSync
+     */
+    protected $checkoutSync;
 
+    /**
+     * @param RequestInterface $request
+     * @param RedirectFactory $redirectFactory
+     * @param CheckoutSync $checkoutSync
+     */
     public function __construct(
-        PageFactory $pageFactory,
         RequestInterface $request,
-        TokenFactory $tokenFactory,
-        CustomerRepositoryInterface $customerRepository,
-        CustomerSession $customerSession,
-        QuoteRepository $quoteRepository,
-        QuoteFactory $quoteFactory,
-        Session $checkoutSession,
-        RedirectFactory $redirectFactory
-    )
-    {
-        $this->pageFactory = $pageFactory;
+        RedirectFactory $redirectFactory,
+        CheckoutSync $checkoutSync
+    ) {
         $this->request = $request;
-        $this->tokenFactory = $tokenFactory;
-        $this->customerRepository = $customerRepository;
-        $this->customerSession = $customerSession;
-        $this->quoteRepository = $quoteRepository;
-        $this->quoteFactory = $quoteFactory;
-        $this->checkoutSession = $checkoutSession;
         $this->redirectFactory = $redirectFactory;
+        $this->checkoutSync = $checkoutSync;
     }
 
     /**
-     * Default customer account page
-     *
-     * @return void
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\Result\Redirect|\Magento\Framework\Controller\ResultInterface
      */
     public function execute()
     {
-        $cartId = $this->request->getParam('cartId');
-        $customerTokenParam = $this->request->getParam('token');
+        $this->checkoutSync->syncCustomerCart(
+            $this->request->getParam(self::SHOGUN_FRONTEND_CHECKOUT_TOKEN_PARAM),
+            $this->request->getParam(self::SHOGUN_FRONTEND_CHECKOUT_CART_PARAM)
+        );
 
-        // 1. Get Token from Token Key
-        $token = $this->tokenFactory->create()->loadByToken($customerTokenParam);
-
-        if (!$token->getId()) {
-            echo "User not found", PHP_EOL;
-            exit;
-        }
-
-        // 2. Get Customer from Token
-        $customer = $this->customerRepository->getById($token->getCustomerId());
-
-        // 3. Login customer
-        $this->customerSession->loginById($customer->getId());
-
-        // 4. Get Customer Quote or Cart Quote
-        try {
-            $quote = $this->quoteRepository->getForCustomer($customer->getId());
-        } catch (NoSuchEntityException $e) {
-            echo "Error: No quote!", $e->getMessage(), PHP_EOL;
-            $quote = $this->quoteFactory->create();
-
-            // TODO: TEST THIS!
-            try {
-                $cartQuote = $this->quoteRepository->getActive($cartId);
-            } catch (NoSuchEntityException $e) {
-                // TODO: REMOVE DEBUG LOG
-                echo "Error: No cart quote!", $e->getMessage(), PHP_EOL;
-                // $this->logger->error($e->getMessage());
-                return $this->redirectFactory->create()->setPath("checkout");
-            }
-
-            // Merge Cart Quote into Customer Quote, if necessary
-            if ($quote->getId() !== $cartQuote->getId()) {
-                $quote->assignCustomerWithAddressChange(
-                    $customer,
-                    $cartQuote->getBillingAddress(),
-                    $cartQuote->getShippingAddress()
-                );
-                $this->quoteRepository->save($quote->merge($cartQuote)->collectTotals());
-            }
-        }
-
-        //5. Sync checkout to quote
-        $quote->collectTotals();
-        $this->checkoutSession->replaceQuote($quote);
-
-        // 6. Redirect to checkout
-        return $this->redirectFactory->create()->setPath("checkout");
+        return $this->redirectFactory->create()->setPath(
+            self::SHOGUN_FRONTEND_CHECKOUT_CHECKOUT_PATH
+        );
     }
 }
---
+++ Service/CheckoutSync.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ *
+ * Copyright © Shogun, Inc. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace Shogun\FrontendCheckout\Service;
+
+use Magento\Checkout\Model\Session;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Integration\Api\UserTokenReaderInterface;
+use Magento\Integration\Model\Oauth\TokenFactory;
+use Magento\Quote\Model\QuoteFactory;
+use Magento\Quote\Model\QuoteRepository;
+
+/**
+ * Syncs customer carts for checkout
+ */
+class CheckoutSync implements CheckoutSyncInterface
+{
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    protected $customerRepository;
+    /**
+     * @var CustomerSession
+     */
+    protected $customerSession;
+    /**
+     * @var QuoteRepository
+     */
+    protected $quoteRepository;
+    /**
+     * @var QuoteFactory
+     */
+    protected $quoteFactory;
+    /**
+     * @var Session
+     */
+    protected $checkoutSession;
+    /**
+     * @var UserTokenReaderInterface
+     */
+    protected $userTokenReader;
+
+    /**
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param CustomerSession $customerSession
+     * @param QuoteRepository $quoteRepository
+     * @param QuoteFactory $quoteFactory
+     * @param Session $checkoutSession
+     * @param UserTokenReaderInterface $userTokenReader
+     */
+    public function __construct(
+        CustomerRepositoryInterface $customerRepository,
+        CustomerSession $customerSession,
+        QuoteRepository $quoteRepository,
+        QuoteFactory $quoteFactory,
+        Session $checkoutSession,
+        UserTokenReaderInterface $userTokenReader
+    ) {
+        $this->customerRepository = $customerRepository;
+        $this->customerSession = $customerSession;
+        $this->quoteRepository = $quoteRepository;
+        $this->quoteFactory = $quoteFactory;
+        $this->checkoutSession = $checkoutSession;
+        $this->userTokenReader = $userTokenReader;
+    }
+
+    /**
+     * Syncs a customer cart for checkout
+     *
+     * @param string $customerToken
+     * @param string $cartId
+     * @return void
+     */
+    public function syncCustomerCart(string $customerToken, string $cartId)
+    {
+        $customer = $this->getCustomer($customerToken);
+        $this->customerSession->loginById($customer->getId());
+
+        $quote = $this->getQuote($customer, $cartId);
+
+        $this->checkoutSession->replaceQuote($quote);
+        $this->checkoutSession->regenerateId();
+    }
+
+    /**
+     * @param string $tokenParam
+     * @return \Magento\Customer\Api\Data\CustomerInterface
+     * @throws NoSuchEntityException
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    private function getCustomer(string $tokenParam): \Magento\Customer\Api\Data\CustomerInterface
+    {
+        $userToken = $this->userTokenReader->read($tokenParam);
+        $customerId = $userToken->getUserContext()->getUserId();
+
+        return $this->customerRepository->getById($customerId);
+    }
+
+    /**
+     * @param \Magento\Customer\Api\Data\CustomerInterface $customer
+     * @param string $cartId
+     * @return \Magento\Quote\Model\Quote
+     */
+    private function getQuote(\Magento\Customer\Api\Data\CustomerInterface $customer, string $cartId): \Magento\Quote\Model\Quote
+    {
+        $quote = $this->getCustomerQuote($customer);
+        $cartQuote = $this->getCartQuote($cartId);
+
+        if ($cartQuote->getId() && ($quote->getId() !== $cartQuote->getId())) {
+            $quote = $quote->merge($cartQuote);
+        }
+
+        if (!$quote->getId()) {
+            $quote->assignCustomer($customer);
+            $quote->collectTotals();
+            $this->quoteRepository->save($quote);
+        }
+
+        return $quote;
+    }
+
+    /**
+     * @param $customer
+     * @return \Magento\Quote\Model\Quote
+     */
+    private function getCustomerQuote($customer): \Magento\Quote\Model\Quote
+    {
+        try {
+            return $this->quoteRepository->getForCustomer($customer->getId());
+        } catch (NoSuchEntityException $e) {
+            return $this->quoteFactory->create();
+        }
+    }
+
+    /**
+     * @param string $cartId
+     * @return \Magento\Quote\Api\Data\CartInterface|\Magento\Quote\Model\Quote
+     */
+    private function getCartQuote(string $cartId)
+    {
+        try {
+            return $this->quoteRepository->getActive($cartId);
+        } catch (NoSuchEntityException $e) {
+            return $this->quoteFactory->create();
+        }
+    }
+}
